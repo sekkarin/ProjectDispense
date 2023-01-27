5c4748d216f8c306e7ab6d1fd5d4153626808c1c
sekkarin <sekkri1234@gmail.com>
2023-01-27 16:18:25 +0700

    ...

--- dispenseApp/screens/managemMdicine/ListMedicine.js
+++ dispenseApp/screens/managemMdicine/ListMedicine.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
 import {Button, Header, Icon, Text} from '@rneui/base';
 import LinearGradient from 'react-native-linear-gradient';
