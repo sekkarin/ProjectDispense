854cd221e9432bc6b8512163f97d91698a746d81
PXTN45 <120036577+PXTN45@users.noreply.github.com>
2023-02-04 22:09:57 +0700

    asd

--- dispenseApp/store/auth-context.js
+++ dispenseApp/store/auth-context.js
@@ -5,18 +5,22 @@ export const AuthContext = createContext({
   isAuthenticate: false,
   authenticate: () => {},
   logout: () => {},
+  USERID: '',
   // userLogin: {},
 });
 
 function AuthContextProvider({children}) {
   const [isLogIn, setLogIn] = useState(false);
+  const [id, setID] = useState('');
 
   async function authenticate(ID) {
     setLogIn(true);
+    setID(ID);
     await AsyncStorage.setItem('IDUSER', ID);
   }
   function logout() {
     setLogIn(false);
+    setID('');
     // setUserLogin({});
     AsyncStorage.setItem('IDUSER', '');
   }
@@ -25,6 +29,7 @@ function AuthContextProvider({children}) {
     isAuthenticate: isLogIn,
     authenticate: authenticate,
     logout: logout,
+    USERID: id,
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
